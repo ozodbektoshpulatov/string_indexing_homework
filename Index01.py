1bdca393e3b82f44b58e0759e28fea23806dec42
ozodbektoshpulatov <toshpulatovozodbek96@gmail.com>
2023-07-17 09:47:14 +0500

    index01

--- Index01.py
+++ Index01.py
@@ -3,7 +3,14 @@ def main(s):
     The string variable s is given. return the first character.
     Args:
         s(str): parameter
-    Returns:
+        Returns:
         str: answer
-    """
-    return
+        """
+    s="parameter"
+    return s[0]
+print(main("parameter"))
+
+    
+
+
+        
